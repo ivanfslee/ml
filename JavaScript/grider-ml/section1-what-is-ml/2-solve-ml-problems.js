83dfda720695369ad60832ba9de49650ddc32091
ivanfslee <ivanfslee@gmail.com>
2020-06-11 01:17:54 -0700

    Added notes on grider ml course intro.

---
+++ JavaScript/grider-ml/section1-what-is-ml/2-solve-ml-problems.js
@@ -0,0 +1,13 @@
+/*
+Asking for help
+    1. udemy QA boards
+    2. twitter @ste_grider
+    3. email: ste.grider@gmail.com
+
+Machine Learning - A Quick Example
+
+    A high level overview on how we would
+        use ML to approach a problem.
+
+
+*/
